d06059e5280a9190fc3c38e86246d4c448b50392
Henning Häcker <henning.haecker+github.com@protonmail.com>
2023-10-05 02:45:44 +0200

    cleanup

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
-from prometheus_client import start_wsgi_server, CollectorRegistry, Gauge
+import logging
 import os
 import threading
 import time
 import random
 import re
 
+from prometheus_client import start_wsgi_server, CollectorRegistry, Gauge
+
 import nextcloud
 
+# Logging level is configurable
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+
+# Default port 8000 is configurable
 HTTP_PORT = int(os.getenv("HTTP_PORT", "8000"))
 
-# store all metrics objects by their name
+# setup logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.ERROR
+)
+logger = logging.getLogger("main")
+logger.setLevel(LOG_LEVEL)
+
+# Store all metric objects by their name for dynamic metric management
 metrics_by_name = {}
 
-# Updates a metric, creates it if non existent yet
-def export_metric(raw_metric_name : str, metric_value : int):
+# Function to export or create a metric with the given name and value
+def export_metric(raw_metric_name: str, metric_value: int):
     metric_name = format_to_valid_metric_name(raw_metric_name)
-    
+
     if metric_name not in metrics_by_name:
         # Create a new metric if it doesn't exist yet
-        metric = Gauge(metric_name, f"fetched by nextcloud-task-automation", registry=custom_registry)
+        metric = Gauge(metric_name, "Metric fetched by nextcloud-task-automation", registry=custom_registry)
         metrics_by_name[metric_name] = metric
 
     # Set the metric value
@@ -27,24 +40,38 @@ def export_metric(raw_metric_name : str, metric_value : int):
 # Function to periodically update the metrics
 def update_metrics():
     while True:
+        # Fetch board information from Nextcloud
         boards = nextcloud.fetch_boards_info()
+
+        # export total number of boards
         boards_total = len(boards)
         export_metric("boards_total", boards_total)
+
         for board in boards:
             board_name = board["name"]
+
+            # Export the total number of lists and cards for each board
             board_lists_total = board["number_of_lists"]
-            export_metric("board_%s_lists_total" % board_name, board_lists_total)
+            export_metric(f"board_{board_name}_lists_total", board_lists_total)
+
             board_cards_total = board["number_of_cards"]
-            export_metric("board_%s_cards_total" % board_name, board_cards_total)
+            export_metric(f"board_{board_name}_cards_total", board_cards_total)
+
             board_lists = board["lists"]
+
             for list_item in board_lists:
                 list_name = list_item["name"]
+
+                # Export the total number of cards for each list within a board
                 list_cards_total = list_item["number_of_cards"]
-                export_metric("board_%s_list_%s_cards_total" % (board_name, list_name), list_cards_total)
-        print(f"Updated {len(metrics_by_name)} metrics")
+                export_metric(f"board_{board_name}_list_{list_name}_cards_total", list_cards_total)
+
+        # Print the number of updated metrics
+        logger.info(f"Updated {len(metrics_by_name)} metrics")
 
         time.sleep(5)
 
+# Function to format a string to a valid metric name
 def format_to_valid_metric_name(input_string):
     # Replace any characters that are not valid in a metric name with underscores
     sanitized_string = re.sub(r'[^a-zA-Z0-9:_]', '_', input_string)
@@ -63,7 +90,9 @@ if __name__ == '__main__':
     # Create a custom Prometheus registry
     custom_registry = CollectorRegistry()
 
-    # Start the Prometheus WSGI server on desired port with the custom registry
+    logger.info(f"Launching HTTP server on port {HTTP_PORT}")
+
+    # Start the Prometheus WSGI server on the specified HTTP_PORT with the custom registry
     start_wsgi_server(HTTP_PORT, addr='0.0.0.0', registry=custom_registry)
 
     # Start a thread to periodically update the metrics
--- nextcloud.py
+++ nextcloud.py
@@ -1,19 +1,14 @@
-import datetime
 import os
 import requests
-import re
-import yaml
-from pprint import pprint
 import logging
 
 LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
 
-# setup logging
+# Setup logging
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.ERROR
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=LOG_LEVEL
 )
-logger = logging.getLogger("log")
-logger.setLevel(LOG_LEVEL)
+logger = logging.getLogger("nextcloud")
 
 NEXTCLOUD_USERNAME = os.getenv("NEXTCLOUD_USERNAME")
 NEXTCLOUD_PASSWORD = os.getenv("NEXTCLOUD_PASSWORD")
@@ -24,62 +19,87 @@ auth = (NEXTCLOUD_USERNAME, NEXTCLOUD_PASSWORD)
 headers = {"Content-Type": "application/json", "OCS-APIRequest": "true"}
 
 def _get_boards():
-    logger.debug("_get_boards()")
+    """
+    Fetches and returns boards from Nextcloud Deck API.
+    """
     response = requests.get(
-            f"{NEXTCLOUD_HOSTNAME}/index.php/apps/deck/api/v1.0/boards",
-            auth = auth,
-            headers=headers)
+        f"{NEXTCLOUD_HOSTNAME}/index.php/apps/deck/api/v1.0/boards",
+        auth=auth,
+        headers=headers,
+    )
     response.raise_for_status()
     return response.json()
 
 def _get_lists(boardId):
-    logger.debug("_get_lists()")
+    """
+    Fetches and returns lists for a given board ID from Nextcloud Deck API.
+    """
     response = requests.get(
-            f"{NEXTCLOUD_HOSTNAME}/index.php/apps/deck/api/v1.0/boards/{boardId}/stacks",
-            auth=auth,
-            headers=headers)
+        f"{NEXTCLOUD_HOSTNAME}/index.php/apps/deck/api/v1.0/boards/{boardId}/stacks",
+        auth=auth,
+        headers=headers,
+    )
     response.raise_for_status()
     return response.json()
 
+def _fetch_board_info(board):
+    """
+    Fetches and processes information about a single board.
+    """
+    board_title = board.get("title")
+    board_id = board.get("id")
+
+    if board_title is None or board_id is None:
+        logger.error("Skipping board with missing title or id")
+        return None
+
+    board_lists = _get_lists(board_id)
+    total_card_count, board_lists_info = _fetch_lists_info(board_lists)
+    return {
+        "name": board_title,
+        "number_of_cards": total_card_count,
+        "number_of_lists": len(board_lists),
+        "lists": board_lists_info,
+    }
+
+def _fetch_lists_info(board_lists):
+    """
+    Fetches and processes information about lists within a board.
+    """
+    total_card_count = 0
+    board_lists_info = []
+
+    for board_list in board_lists:
+        list_title = board_list.get("title")
+        list_id = board_list.get("id")
+        list_cards = board_list.get("cards", [])
+        list_card_count = len(list_cards)
+
+        board_lists_info.append({"name": list_title, "number_of_cards": list_card_count})
+        total_card_count += list_card_count
+
+    return total_card_count, board_lists_info
+
 def fetch_boards_info():
+    """
+    Fetches and processes information about all boards.
+    """
     boards = _get_boards()
     results_boards = []
-    board_id = None
-    automation_board = None
-    total_boards = len(boards)
     total_lists = 0
     total_cards = 0
-    for board in boards:
-        total_board_cards = 0
-        board_title = board.get("title")
-        if board_title == None:
-            logger.error("board has no title")
-        board_id = board.get("id")
-        if board_id == None:
-            logger.error("board has no id")
-        logger.debug("- %s", board_title)
-        board_lists = _get_lists(board_id)
-        total_lists += len(board_lists)
-        new_board_lists_item = []
-        for board_list in board_lists:
-            list_title = board_list.get("title")
-            if list_title == None:
-                logger.error("list has no title")
-            list_id = board_list.get("id")
-            if list_id == None:
-                logger.error("list has no id")
-            list_cards = board_list.get("cards")
-            if list_cards == None:
-                logger.debug("list is empty")
-                list_cards = []
-            new_board_lists_item.append({"name": list_title, "number_of_cards": len(list_cards)})
-            logger.debug("List '%s' has %i cards", list_title, len(list_cards))
-            total_board_cards += len(list_cards)
-            total_cards += len(list_cards)
 
-        results_boards_item = {"name": board_title, "number_of_cards": total_board_cards, "number_of_lists": len(board_lists), "lists": new_board_lists_item}
-        results_boards.append(results_boards_item)
-        logger.debug("Board '%s' has %i lists and %i cards in total", board_title, len(board_lists), total_board_cards)
-    logger.info("Found %i boards with %i lists and %i cards", len(boards), total_lists, total_cards)
+    for board in boards:
+        board_info = _fetch_board_info(board)
+        if board_info:
+            results_boards.append(board_info)
+
+    for board_info in results_boards:
+        total_lists += board_info["number_of_lists"]
+        total_cards += board_info["number_of_cards"]
+
+    logger.debug(
+        "Found %i boards with %i lists and %i cards", len(boards), total_lists, total_cards
+    )
 
     return results_boards
